5490a0d66514922f54477ad7e9ae52050458a17d
Yusuke Niitani <yuyuniitani@gmail.com>
2017-05-17 17:00:33 +0900

    update training protocol

--- examples/faster_rcnn/experiments/vgg16_voc07_voc12.sh
+++ examples/faster_rcnn/experiments/vgg16_voc07_voc12.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+# Train protocol follows
+# https://github.com/endernewton/tf-faster-rcnn/blob/master/experiments/scripts/train_faster_rcnn.sh#L26
+
 cd ..
-python train_faster_rcnn.py --gpu 0 --dataset voc07+voc12 --iteration 210000 --out result/vgg16_voc07_voc12 --lr 0.001 --seed 0 --step_size 170000
+python train_faster_rcnn.py --gpu 0 --dataset voc07+voc12 --iteration 110000 --out result/vgg16_voc07_voc12 --lr 0.001 --seed 0 --step_size 80000
 
 python eval.py 0 result/vgg16_voc07_voc12/model
